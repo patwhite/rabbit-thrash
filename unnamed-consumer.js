081fee5b323da5d0e100401525dd1f48c2fc13c0
Pat White <patwhite@cisco.com>
2017-10-09 03:31:32 +0000

    Adding unnamed consumer

---
+++ unnamed-consumer.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+var node = process.argv[4] || "0";
+console.log("Connecting to node: " + node);
+
+var conn = 'amqp://user:BGJLo2pmiyx5@rabbitmq-cluster-1-node-' + node;
+var channel;
+
+
+var connectCount = parseInt(process.argv[2]);
+console.log("Connection count: " + connectCount);
+
+var maxTotalRecipients = parseInt(process.argv[3]);
+console.log("Total recipient count: " + maxTotalRecipients);
+
+function init(ch) {
+    var ex = 'app';
+    ch.assertExchange(ex, 'direct', {durable: false});
+
+    for(var i = 0; i < connectCount; i++) {
+        const whoami = getRandomInt(0, maxTotalRecipients).toString();
+
+        ch.assertQueue('', {expires: 60000, exclusive: true}, function(err, q) {
+
+            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+
+            ch.bindQueue(q.queue, ex, whoami);
+
+            ch.consume(q.queue, function(msg) {
+//                console.log(" [x] %s", msg.content.toString());
+            }, {noAck: true});
+        });
+    }
+
+    console.log("Finished asserting and binding.");
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+amqp.connect(conn, function(err, conn) {
+    conn.createChannel(function(err, ch) {
+        channel = ch;
+        init(ch);
+    });
+});
